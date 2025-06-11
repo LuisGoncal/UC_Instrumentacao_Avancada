c48463c94238b4d625d2b9f392b3b5ecf93a513b
Luís Manuel Mendonça Gonçalves <91914598+LuisGoncal@users.noreply.github.com>
2025-06-11 14:09:42 +0100

    Create teds4.h

---
+++ PIC18F47Q10-Sensor/avaliaçao.X/teds4.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   teds.h
+ * Author: USER
+ *
+ * Created on 30 de Novembro de 2023, 9:54
+ */
+
+#ifndef TEDS_H
+#define	TEDS_H
+#ifndef XC_HEADER_TEMPLATE_H
+#define	XC_HEADER_TEMPLATE_H
+#include <xc.h>
+
+#endif	/* TEDS_H */
+
+
+
+typedef struct METATEDS_TEMPLATE {
+    uint8_t TEDSID[6];
+    uint8_t UUID[12];
+    
+
